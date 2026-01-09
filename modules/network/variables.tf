31986908f9ede19a29e7c3c023bb23d21b6b334d
sumandhakal04 <dhakalsuman04@gmail.com>
2026-01-09 12:00:30 +0000

    updated variable names for underscore

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -4,13 +4,13 @@ variable "vpc_name" {}
 
 variable "vpc_cidr_block" {}
 
-variable "public_subnet-1" {}
+variable "public_subnet_1" {}
 
-variable "public_subnet-2" {}
+variable "public_subnet_2" {}
 
-variable "private_subnet-1" {}
+variable "private_subnet_1" {}
 
-variable "private_subnet-2" {}
+variable "private_subnet_2" {}
 
 variable "azs" {
   type = list(string)
